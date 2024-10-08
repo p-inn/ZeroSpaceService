aaedc54b55f0817bd1d3e1d4e8fce155002fd88a
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2024-10-08 23:25:57 +0900

    chore: CORS 에러 확인을 위한 로그인 버튼 임시 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
 // import Image from "next/image";
 
-import Calendar from "@/components/Calendar";
-import Sidebar from "@/components/Sidebar";
+// import Calendar from "@/components/Calendar";
+// import Sidebar from "@/components/Sidebar";
+import LoginButton from "@/components/LoginButton";
 
 export default function Home() {
+  // return (
+  //   <div>
+  //     <h1>
+  //       <Sidebar></Sidebar>
+  //     </h1>
+  //     <h1>
+  //       <Calendar></Calendar>
+  //     </h1>
+  //     <h1>
+  //       <LoginButtontton></LoginButton>
+  //     </h1>
+  //   </div>
+  // );
   return (
-    <div>
-      <h1>
-        <Sidebar></Sidebar>
-      </h1>
-      <h1>
-        <Calendar></Calendar>
-      </h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold text-center mb-6">환영합니다</h1>
+        <p className="text-center mb-6">로그인하세요</p>
+        <LoginButton />
+      </div>
     </div>
   );
 }
