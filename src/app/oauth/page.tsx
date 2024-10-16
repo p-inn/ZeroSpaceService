849ec0bc213546e3a9e9ce7101fad28ff6a17c90
Pinn <lsol1130@naver.com>
2024-10-17 01:46:45 +0900

    [fix] Suspense 추가

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { OAuthRedirect } from "@/components/OAuthRedirect";
+import { Suspense } from "react";
 
 export default function OAuthRedirectPage() {
-  return <OAuthRedirect />;
+  return (
+    <Suspense fallback={<div>로그인 중입니다... 잠시만 기다려 주세요.</div>}>
+      <OAuthRedirect />
+    </Suspense>
+  );
 }
