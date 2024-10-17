15e4058be77c3a338eb0bdd452294623ecb15c14
Pinn <lsol1130@naver.com>
2024-10-17 21:26:06 +0900

    [feat] 회원탈퇴 API 연동

--- src/components/SetSignIn.tsx
+++ src/components/SetSignIn.tsx
@@ -1,17 +1,36 @@
 "use client";
 
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { userState } from "@/recoil/atoms";
 import useSignOutQuery from "@/app/hooks/auth/useLogoutQuery";
+import useWithDrawQuery from "@/app/hooks/auth/useWithDrawQuery";
 
 const LoggedInView: React.FC = () => {
-  const user = useRecoilValue(userState);
+  const [user, setUserState] = useRecoilState(userState);
   const { signOutMutate } = useSignOutQuery();
+  const { withDrawMutate } = useWithDrawQuery();
 
   const handleLogout = () => {
     signOutMutate();
+    // Recoil 상태 초기화
+    setUserState({
+      isAuthenticated: false,
+      accessToken: "",
+      email: "",
+    });
   };
+
+  const handleWithDraw = () => {
+    withDrawMutate();
+    // Recoil 상태 초기화
+    setUserState({
+      isAuthenticated: false,
+      accessToken: "",
+      email: "",
+    });
+  };
+
   return (
     <div>
       <h3 className="my-2 font-semibold">로그인 정보</h3>
@@ -21,12 +40,15 @@ const LoggedInView: React.FC = () => {
       <div className="w-full bg-gray-300 p-2 rounded-lg">
         <span className="text-gray-700">{user.email}</span>
       </div>
-      <div className="flex gap-1">
-        <button className="btn bg-white text-gray-500 px-4 py-2 rounded-3xl hover:text-blue hover:border-blue-500 hover:bg-blue-300">
+      <div className="flex gap-3 justify-center">
+        <button
+          className="btn w-full bg-white text-gray-500 px-4 py-2 rounded-3xl hover:text-blue-500 hover:border-blue-500 hover:bg-blue-200"
+          onClick={handleWithDraw}
+        >
           회원탈퇴
         </button>
         <button
-          className="btn bg-white text-gray-500 px-4 py-2 rounded-3xl hover:text-blue hover:border-blue-500 hover:bg-blue-300"
+          className="btn w-full bg-white text-gray-500 px-4 py-2 rounded-3xl hover:text-blue-500 hover:border-blue-500 hover:bg-blue-200"
           onClick={handleLogout}
         >
           로그아웃
