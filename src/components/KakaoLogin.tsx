1a26199324656c053bc178e006449d2796896a6e
Pinn <lsol1130@naver.com>
2024-10-22 19:22:11 +0900

    [add]약관 관련 링크 추가

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { BsChevronRight } from "react-icons/bs";
+
 declare global {
   interface Window {
     Kakao: any;
@@ -19,9 +21,15 @@ const KakaoLoginButton: React.FC = () => {
       >
         <p>카카오톡으로 3초만에 로그인</p>
       </button>
-      <span className="text-gray-500 text-sm">
+      <span className="text-gray-500 text-sm mt-2">
         회원가입 시 제로스페이스의 이용약관 / 개인정보처리 방침에 동의하게
         됩니다.
+        <a 
+        className="link link-hover"
+        href="https://hickory-mandible-ea4.notion.site/127b5ea5a7df80bfa11bcefe37fa57f5"
+      >
+        약관보기 <BsChevronRight />
+      </a>
       </span>
     </>
   );
